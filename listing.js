22ed5e04777f7a44598e784dac427d80848c1dc9
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-05-01 14:02:13 +1000

    Push new code

--- listing.js
+++ listing.js
@@ -1,61 +1,60 @@
-/*// Make sure you're not using IE in 2020
-const ua = window.navigator.userAgent;
-const msie = ua.indexOf("MSIE ");
-
-if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv:11\./)) window.location.href = "/iebad.html";
-*/ // Commented this out, since the corresponding page (iebad.html) was removed, and it only served as personal bias. -- omame
-
-let productList = {};
-
 const typeObj = document.querySelector("#type");
 const versionObj = document.querySelector("#version");
 const editionObj = document.querySelector("#edition");
 const downloadURL = document.querySelector("#download");
 
-fetch("/products.json").then(data => data.json().then(products => {
-	productList = products;
-	for (const i in products) {
-		typeObj.append(new Option(i, i));
+function getData(d){return fetch(d).then(a=>a.body.getReader()).then(a=>{return new Response(new ReadableStream({start(b){let c=()=>{a.read().then(({done,value})=>{if(done)return b.close();b.enqueue(value);c()})};c()}})).text()})}
+
+async function getListing(folder = "", obj = typeObj) {
+	let links = await getData(`${debugProxy}https://dl.bobpony.com/${folder}`).then(a => {
+		let dir = document.createElement("div");
+		dir.innerHTML = a;
+		return dir.getElementsByTagName("a");
+	});
+	for (var l = 0; l < links.length; l++) {
+		if (obj == editionObj) {
+			if (!links[l].innerText.startsWith(".."))
+				if (links[l].innerText.endsWith("/")) {
+					let innerDir = await getListing(`${folder}/${links[l].innerText}`, obj);
+					for (var il = 0; il < innerDir.length; il++) {
+						if (!innerDir[il].innerText.startsWith("..") & innerDir[il].innerText != "_h5ai.header.html")
+							obj.append(new Option(innerDir[il].innerText.substring(0,innerDir[il].innerText.lastIndexOf(".")), innerDir[il].innerText));
+					}
+				}
+				else if(links[l].innerText != "_h5ai.header.html")
+						obj.append(new Option(links[l].innerText.substring(0,links[l].innerText.lastIndexOf(".")), links[l].innerText));
+		} else if (links[l].innerText.endsWith("/") & !links[l].innerText.startsWith(".."))
+			obj.append(new Option(links[l].innerText.replace("/",""),links[l].innerText.replace("/","")));
 	}
-}));
+}
 
-const updateVersions = () => {
+function updateVersions() {
 	const versionOptions = document.querySelectorAll("#version option");
-
 	for (const i of versionOptions) {
 		if (!i.disabled) i.remove();
 	}
-
 	document.querySelector("#version option[value='placeholder']").selected = true;
-
-	for (const i in productList[typeObj.value]) {
-		versionObj.append(new Option(i, i));
-	}
-
+	getListing(typeObj.value, versionObj);
 	updateEditions();
-};
+}
 
-const updateEditions = () => {
+function updateEditions() {
 	let editionOptions = document.querySelectorAll("#edition option");
 	for (let i of editionOptions) {
 		if (!i.disabled) i.remove();
 	}
-
 	document.querySelector("#edition option[value='placeholder']").selected = true;
-
-	for (let i in productList[typeObj.value][versionObj.value]) {
-		editionObj.append(new Option(i, i));
-	}
-
+	getListing(typeObj.value+"/"+versionObj.value, editionObj);
 	updateURL();
-};
+}
 
-const updateURL = () => {
+function updateURL() {
 	if (typeObj.value && versionObj.value && editionObj.value) {
-		const url = productList[typeObj.value][versionObj.value][editionObj.value];
-		downloadURL.href = url;
+		downloadURL.href = `https://dl.bobpony.com/${typeObj.value}/${versionObj.value}/${editionObj.value}`;
 	} else {
 		downloadURL.removeAttr("href");
 		downloadURL.disabled = true;
 	}
-};
+}
+
+getListing();
