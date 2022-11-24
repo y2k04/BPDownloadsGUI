6045ecc7974c7be97123e1f1bddd3710fd951bcf
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-11-24 10:08:37 +1000

    Use AllOrigins to bypass CORS

--- listing.js
+++ listing.js
@@ -6,7 +6,7 @@ const downloadURL = document.querySelector("#download");
 function getData(d){return fetch(d).then(a=>a.body.getReader()).then(a=>{return new Response(new ReadableStream({start(b){let c=()=>{a.read().then(({done,value})=>{if(done)return b.close();b.enqueue(value);c()})};c()}})).text()})}
 
 async function getListing(folder = "", obj = typeObj) {
-	let links = await getData(`https://dl.bobpony.com/${folder}`).then(a => {
+	let links = await getData(`https://api.allorigins.win/raw?url=https://dl.bobpony.com/${folder}`).then(a => {
 		let dir = document.createElement("div");
 		dir.innerHTML = a;
 		return dir.getElementsByTagName("a");
